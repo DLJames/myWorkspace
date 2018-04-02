31a8f12a3b28863aa5bdd80bcdadc7d467f375b4
dlshbn <dlshbn@163.com>
2018-04-02 19:25:26 +0800

    add echarts component

--- JMS/src/components/DashBoard.vue
+++ JMS/src/components/DashBoard.vue
@@ -1,11 +1,18 @@
 <template>
     <div class="jms-dashboard jms-scrollBar">
-        <div class="jms-dashboard-header">DashBoard</div>
+        <div class="jms-dashboard-chart">
+            <div class="jms-chartTitle">DashBoard</div>
+            <div id="jms-myChart" class="jms-myChart"></div>
+        </div>
     </div>
 </template>
 
 <script>
 /* eslint-disable */
+// import echarts from 'echarts/lib/echarts'
+// import bar from 'echarts/lib/chart/bar'
+// import tooltip from 'echarts/lib/component/tooltip'
+// import title from 'echarts/lib/component/title'
 export default {
     name: 'Dashboard',
     data() {
@@ -14,16 +21,59 @@ export default {
         }
     },
     mounted() {
-        // let arr = Array.apply(null, {length:200});
-        // var domNode = document.querySelector('.jms-dashboard');
-        // arr.forEach((element, idx) => {
-        //     var d = document.createElement('div');
-        //     d.innerHTML  = idx;
-        //     domNode.appendChild(d);
-        // });
+        this.drawLine();
     },
     methods: {
-
+        drawLine() {
+            var myChart = this.$echarts.init(document.getElementById('jms-myChart'), 'dark');
+            var option = {
+                title: {
+                    text: 'My JMS Dashboard'
+                },
+                // backgroundColor: 'black',
+                // textStyle: {
+                //     color: 'red'
+                // },
+                legend: {
+                    data: ['Time']
+                },
+                tooltip:{
+                    trigger: 'axis',
+                    axisPointer: {
+                        type: 'cross',
+                        label: {
+                            backgroundColor: '#283b56'
+                        }
+                    }
+                },
+                xAxis: {name: 'Week', data: []},
+                yAxis: {name: 'Num'},
+                series: [{
+                    name: 'Time',
+                    type: 'bar',
+                    color: ['yellow', 'red'],
+                    data: []
+                }]
+            }
+            myChart.setOption(option);
+            myChart.showLoading({
+                    color: '#e5e5e5',
+                    textColor: '#e5e5e5',
+                    maskColor: 'rgba(250, 250, 250, 0.3)'
+                });
+            setTimeout(()=>{
+                myChart.hideLoading();
+                myChart.setOption({
+                    xAxis: {
+                        data: ['Mon', 'Tues', 'Wed', 'Thurs', 'Fri', 'Sat', 'Sun']
+                    },
+                    series: [{
+                        name: 'Time',
+                        data: [6, 20, 38, 18, 17, 9, 1]
+                    }]
+                });
+            }, 3000);
+        }
     }
 }
 </script>
@@ -42,7 +92,15 @@ export default {
         background: antiquewhite;
         font-size: 35px;
         display: flex;
+        flex-direction: column;
         justify-content: center;
         align-items: center;
+    }
+    .jms-dashboard-chart{
+
+    }
+    .jms-dashboard .jms-dashboard-chart .jms-myChart{
+        width: 100%;
+        height: 300px;;
     }
 </style>
--- JMS/src/main.js
+++ JMS/src/main.js
@@ -2,12 +2,13 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 // import App from './App'
+import echarts from 'echarts'
 import router from './router'
 import Header from './components/Header'
 import DashBoard from './components/DashBoard'
 
-Vue.config.productionTip = false
-
+Vue.config.productionTip = false;
+Vue.prototype.$echarts = echarts;
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
